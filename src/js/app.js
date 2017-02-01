d793e3b8fc9d266e46a6b98b80c348831e928b3c
Serhii <kso_132@mail.ru>
2017-02-01 18:49:02 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+// example of simple includes for js
+//=include lib/sayHello.js
+//=include lib/jquery.min.js
+//=include lib/slick.min.js
+//=include lib/svgxuse.min.js
+
+sayHello();
+
+$(document).ready(function(){
+
+$('.js-slider').slick({
+	arrows: true
+});
+//////slick
+
+
+
+
+	///////////
+});
