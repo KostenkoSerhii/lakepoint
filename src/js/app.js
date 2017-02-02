61c28eeaca93bfef4b250b3e233c0d541acae9e3
Serhii <kso_132@mail.ru>
2017-02-02 18:10:31 +0200

    gamburger in progress

--- src/js/app.js
+++ src/js/app.js
@@ -8,21 +8,30 @@ sayHello();
 
 $(document).ready(function(){
 
-$('.js-slider').slick({
-	arrows: true
-});
+	$('.js-slider').slick({
+		arrows: true
+	});
 //////slick
 
 //var hasSubMenu = $(".menu__item").children(".sub-menu");
 $(".menu__item").on("click", function(){
-	$(".menu__item").not(this).removeClass("js-menu-active");
-	$(this).addClass("js-menu-active");
+	var $this = $(this);
+	$(".menu__item").not($this).removeClass("js-menu-active");
+	$($this).addClass("js-menu-active");
 	//if($(this).children(".sub-menu")){
-		$(this).children(".sub-menu").slideToggle();
+		$(".menu__item").not($this).children(".sub-menu").slideUp();
+		$($this).children(".sub-menu").slideToggle();
 	//}
-		
-	});
 
+});
+
+$(".menu-btn").on("click", function(){
+	$(this).toggleClass("btn-is-active");
+	$(".header__menu").slideToggle();
+	$(".menu__item").removeClass("menu-btn");
+	$(".menu__item").children(".sub-menu").slideUp();
+
+});
 
 
 	///////////
